76b88f2ed421d5b64bbffd81e035be13c776deb7
Mohd Alquama <mohdalquama1@gmail.com>
2025-01-13 17:10:54 +0530

    import bcrypt from 'bcryptjs';

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import bcrypt from "bcrypt";
+import bcrypt from 'bcryptjs';
+
 import jwt from "jsonwebtoken";
 import User from '../models/user.js';
 import dotenv from "dotenv";
